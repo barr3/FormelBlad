02e8d53f5b91149e406e8b0d7d199a29ff595024
barr3 <barre@novigrad.localadmin>
2021-03-26 23:16:32 +0100

    Added OOP.

--- js/main.js
+++ js/main.js
@@ -3,18 +3,29 @@
 let formulas = [];
 
 class Formula {
-    constructor(formula, explainaiton, vars, id, obj) {
+    constructor(formula, explainaiton, vars, ids) {
 
 	this.formula = formula;
 	this.explaination = explainaiton;
-	this.id = id;
-	this.obj = obj;
+	this.ids = ids;
+	this.vars = vars;
+	this.selectedId;
+	this.obj = [];
 
+	for (var i = 0; i < this.ids.length; i++) {
+	    this.obj.push(document.getElementById(this.ids[i]));	    
+	}
+	
+	
+
+	// this.id = this.obj.parentElement.parentElement.id;
+
+	
 	this.numOfVars = vars.length;
 
-	for (var i = 0; i < this.numOfVars; i++) {
-	    this.vars[i] = vars[i];
-	}
+	// for (var i = 0; i < this.numOfVars; i++) {
+	//     this.vars[i] = vars[i];
+	// }
 
 	// this.createRest();
 
@@ -23,7 +34,7 @@ class Formula {
     // createRest() {
     // 	document.getElementById("0").innerHTML += "<div class='Formula'>  <p>"+ this.formula  + "</p>  <p>" + this.explaination  + " </p>       </div>";
     // }
-
+    
     getOtherVars(e){
 
 	for (var i = 0; i < this.vars.length; i++) {
@@ -43,32 +54,74 @@ class Formula {
 
 
 
-    getInput(nr, obj, variable){
+    getInput(variable){
 	console.log(this.obj.id);
 	console.log(variable);
+
+	var main = document.getElementById(this.selectedId).parentElement.parentElement;
 
 	if (clickable == false) {
 
 	    for (var i = 0; i < this.numOfVars ; i++) {
 		
 	    }
-	    
-	    document.getElementById(this.id).insertAdjacentHTML("beforeend", '<div id="temp">  <input type="text" class="tempinput" id="tempinput" name="formula"><button class="tempbutton" onclick="removeField('+ this.obj.id +')">Lös för '+ variable +' </button> </div>');
+	    console.log("här händer det grejer ");
+	    main.insertAdjacentHTML("beforeend", '<div id="temp">  <input type="text" class="tempinput" id="tempinput" name="formula"><button class="tempbutton" onclick="removeField(this.parentElement)">Lös för '+ variable +' </button> </div>');
 	} 
-	
     }
 
+    
+    removeField(){
+
+	// console.log(item.id);
+
+	var content = document.getElementById("tempinput").value; 
+   
+	console.log(content);
+    
+	document.getElementById(this.obj.id).classList.remove("toggle");
+	document.getElemnentById("temp").remove();
+
+	clickable = true;
+    }
 
 }
 
 
-// let einstein = new Formula("\[ E = mc^2  \]", "$E$ är energin, $m$ är massan, och $c är ljusets hastighet$ ");
+formulas.push(new Formula("\[ E = mc^2 \]", "korv", ["E", "m"], ["i","ii"]));
+
+
+
 var clickable = true;
 
 function unknown(obj){
     
     if (clickable == true) {
 	clickable = false;
+	var clickedObject; 
+
+	for (var i = 0; i < formulas.length; i++) {
+
+	    for (var j = 0; j < formulas[i].obj.length; j++) {
+
+		
+		if (formulas[i].obj[j] == obj) {
+		    clickedObject = formulas[i];
+		    console.log("korbscvpa");
+		    clickedObject.selectedId = clickedObject.obj[j].id;
+		}
+		
+	    }
+
+	    
+	}
+	
+	
+	// formulas.forEach(element => if (element == formulas[i].obj) {
+	    
+	// })
+
+
 	obj.classList.add("toggle");
 	var variable = obj.children[0].className;
 	var formula = variable.split(" ")[1];
@@ -81,14 +134,22 @@ function unknown(obj){
 
 	// console.log(variable);
 	// console.log(formula);
-    
-	switch (formula){
 
-	case "einstein":
-	    // console.log("Einsteincheck");
-	    einstein(nr, obj, variable);
+
+
+	// console.log(clickedObject.formula);
+	// console.log(variable);
+	// console.log(clickedObject);
 	
-	}
+	clickedObject.getInput(variable);
+	
+	// switch (formula){
+
+	// case "einstein":
+	//     // console.log("Einsteincheck");
+	//     einstein(nr, obj, variable);
+	
+	// }
 	
     } else {
 	obj.classList.remove("toggle");
@@ -118,16 +179,14 @@ function einstein(nr, obj, variable){
 }
 
 
-function getInput(nr, obj, variable){
-    console.log(obj.id);
-    console.log(variable);
+// function getInput(nr, obj, variable){
 
-    if (clickable == false) {
-	document.getElementById(nr).insertAdjacentHTML("beforeend", '<div id="temp">  <input type="text" class="tempinput" id="tempinput" name="formula"><button class="tempbutton" onclick="removeField('+ obj.id +')">Lös för '+ variable +' </button> </div>');
-    } 
+//     if (clickable == false) {
+// 	document.getElementById(nr).insertAdjacentHTML("beforeend", '<div id="temp">  <input type="text" class="tempinput" id="tempinput" name="formula"><button class="tempbutton" onclick="removeField('+ obj.id +')">Lös för '+ variable +' </button> </div>');
+//     } 
 
 	
-}
+// }
 
 
 function removeField(item){
