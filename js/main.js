c183dd7e4275ecaf3439d8ef15320b5d7ad7105c
barr3 <barre@novigrad.localadmin>
2021-03-28 10:31:29 +0200

    Tried implementing latex math to no avail

--- js/main.js
+++ js/main.js
@@ -9,36 +9,34 @@ class Formula {
 	this.explaination = explainaiton;
 	this.ids = ids;
 	this.vars = vars;
-	this.selectedId;
+	// this.selectedId; //not sure if unused
 	this.obj = [];
 
-	for (var i = 0; i < this.ids.length; i++) {
-	    this.obj.push(document.getElementById(this.ids[i]));	    
+	this.formulaTest = formula.split("[ ")[1].split(" ]")[0];
+	this.formulaTest = this.formulaTest.replace(/\s/g, "");
+	
+
+
+	for (var i = 0; i < this.formulaTest.length; i++) {
+	    // console.log(this.formulaTest[i]);
 	}
 	
 	
-
-	// this.id = this.obj.parentElement.parentElement.id;
-
+	//Takes all ids and inserts the corresponding DOM-object in the obj array.
+	for (var i = 0; i < this.ids.length; i++) { 
+	    this.obj.push(document.getElementById(this.ids[i]));	    
+	}
 	
 	this.numOfVars = vars.length;
 
-	// for (var i = 0; i < this.numOfVars; i++) {
-	//     this.vars[i] = vars[i];
-	// }
-
-	// this.createRest();
-
     }
 
-    // createRest() {
-    // 	document.getElementById("0").innerHTML += "<div class='Formula'>  <p>"+ this.formula  + "</p>  <p>" + this.explaination  + " </p>       </div>";
-    // }
-    
+    //Returns all the variables that are not selected by the user
+    //Not implemented yet.
     getOtherVars(e){
 
 	for (var i = 0; i < this.vars.length; i++) {
-
+	    
 	    if (this.vars[i] != e) {
 		this.notvars[i] = this.vars[i];
 	    }
@@ -48,46 +46,338 @@ class Formula {
     }
 
     
-    formulaLatex(){
+    formulaLatex(content){
+	// for (var i = 0; i < this.vars.length; i++) {
+	//     console.log(this.vars[i]);
+	// }
 
+	var calc = new Calc(this.formulaTest);
+
+	var test = calc.eval(content);
+	console.log(test);
+
+	
     }
 
-
-
+    //Detects what unknown variable is pressed and inserts a textbox to get user input. 
     getInput(variable){
 
 	var main = document.getElementById(this.selectedId).parentElement.parentElement;
 
 	if (clickable == false) {
 
-	    for (var i = 0; i < this.numOfVars ; i++) {
-		
-	    }
 	    var tempId = JSON.stringify(this.ids);
 	    main.insertAdjacentHTML("beforeend", `<div id='temp'>  <input type='text' class='tempinput' id='tempinput' name='formula'><button class='tempbutton' onclick='removeField(${tempId})'>Lös för ${variable} </button> </div>`);
 
 	} 
     }
 
-    
-    removeField(){
+    //Removes the textbox and returns the data of the input.
+    // removeField(){
 
-	// console.log(item.id);
+    // 	// console.log(item.id);
 
-	var content = document.getElementById("tempinput").value; 
+    // 	var content = document.getElementById("tempinput").value; 
    
-	console.log(content);
+    // 	console.log(content);
     
-	document.getElementById(this.obj.id).classList.remove("toggle");
-	document.getElemnentById("temp").remove();
+    // 	document.getElementById(this.obj.id).classList.remove("toggle");
+    // 	document.getElemnentById("temp").remove();
 
-	clickable = true;
-    }
+    // 	clickable = true;
+    // 	return content;
+    // }
 
 }
 
+//================================================================================================================
 
-formulas.push(new Formula("\[ E = mc^2 \]", "korv", ["E", "m"], ["i","ii"]));
+formulas.push(new Formula("\[ x^2 \]", "korv", ["E", "m"], ["i","ii"]));
+
+formulas.push(new Formula("\[  1+y   \]", "korv", ["F", "m", "a"], ["iii","iv", "v"]));
+
+//================================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//_-------------------------------------------------------------------------------------------------------------
+
+
+
+var CALC_CONST = {
+  // define your constants
+  e: Math.E,
+  pi: Math.PI
+};
+
+var CALC_NUMARGS = [
+  [/^(\^|\*|\/|\+|\-)$/, 2],
+  [/^(floor|ceil|(sin|cos|tan|sec|csc|cot)h?)$/, 1]
+];
+
+var Calc = function(expr, infix) {
+  this.valid = true;
+  this.expr = expr;
+
+  if (!infix) {
+    // by default treat expr as raw latex
+    this.expr = this.latexToInfix(expr);
+  }
+
+  var OpPrecedence = function(op) {
+    if (typeof op == "undefined") return 0;
+
+    return op.match(/^(floor|ceil|(sin|cos|tan|sec|csc|cot)h?)$/) ? 10
+
+         : (op === "^") ? 9
+         : (op === "*" || op === "/") ? 8
+         : (op === "+" || op === "-") ? 7
+
+         : 0;
+  };
+
+  var OpAssociativity = function(op) {
+    return op.match(/^(floor|ceil|(sin|cos|tan|sec|csc|cot)h?)$/) ? "R" : "L";
+  };
+
+  var numArgs = function(op) {
+    for (var i = 0; i < CALC_NUMARGS.length; i++) {
+      if (CALC_NUMARGS[i][0].test(op)) return CALC_NUMARGS[i][1];
+    }
+    return false;
+  };
+
+  this.rpn_expr = [];
+  var rpn_expr = this.rpn_expr;
+
+  this.expr = this.expr.replace(/\s+/g, "");
+
+  // This nice long regex matches any valid token in a user
+  // supplied expression (e.g. an operator, a constant or
+  // a variable)
+  var in_tokens = this.expr.match(/(\^|\*|\/|\+|\-|\(|\)|[a-zA-Z0-9\.]+)/gi);
+  var op_stack = [];
+
+  in_tokens.forEach(function(token) {
+    if (/^[a-zA-Z]$/.test(token)) {
+      if (CALC_CONST.hasOwnProperty(token)) {
+        // Constant. Pushes a value onto the stack.
+        rpn_expr.push(["num", CALC_CONST[token]]);
+      }
+      else {
+        // Variables (i.e. x as in f(x))
+        rpn_expr.push(["var", token]);
+      }
+    }
+    else {
+      var numVal = parseFloat(token);
+      if (!isNaN(numVal)) {
+        // Number - push onto the stack
+        rpn_expr.push(["num", numVal]);
+      }
+      else if (token === ")") {
+        // Pop tokens off the op_stack onto the rpn_expr until we reach the matching (
+        while (op_stack[op_stack.length - 1] !== "(") {
+          rpn_expr.push([numArgs(op_stack[op_stack.length - 1]), op_stack.pop()]);
+          if (op_stack.length === 0) {
+            this.valid = false;
+            return;
+          }
+        }
+
+        // remove the (
+        op_stack.pop();
+      }
+      else if (token === "(") {
+        op_stack.push(token);
+      }
+      else {
+        // Operator
+        var tokPrec = OpPrecedence(token),
+           headPrec = OpPrecedence(op_stack[op_stack.length - 1]);
+
+        while ((OpAssociativity(token) === "L" && tokPrec <= headPrec) ||
+          (OpAssociativity(token) === "R" && tokPrec < headPrec)) {
+
+          rpn_expr.push([numArgs(op_stack[op_stack.length - 1]), op_stack.pop()]);
+          if (op_stack.length === 0) break;
+
+          headPrec = OpPrecedence(op_stack[op_stack.length - 1]);
+        }
+
+        op_stack.push(token);
+      }
+    }
+  });
+
+  // Push all remaining operators onto the final expression
+  while (op_stack.length > 0) {
+    var popped = op_stack.pop();
+    if (popped === ")") {
+      this.valid = false;
+      break;
+    }
+    rpn_expr.push([numArgs(popped), popped]);
+  }
+};
+
+/*
+ * returns the result of evaluating the current expression
+ */
+Calc.prototype.eval = function(x) {
+  var stack = [], rpn_expr = this.rpn_expr;
+
+  rpn_expr.forEach(function(token) {
+    if (typeof token[0] == "string") {
+      switch (token[0]) {
+        case "var":
+          // Variable, i.e. x as in f(x); push value onto stack
+          //if (token[1] != "x") return false;
+          stack.push(x);
+          break;
+
+        case "num":
+          // Number; push value onto stack
+          stack.push(token[1]);
+          break;
+      }
+    }
+    else {
+      // Operator
+      var numArgs = token[0];
+      var args = [];
+      do {
+        args.unshift(stack.pop());
+      } while (args.length < numArgs);
+
+      switch (token[1]) {
+        /* BASIC ARITHMETIC OPERATORS */
+        case "*":
+          stack.push(args[0] * args[1]);
+          break;
+        case "/":
+          stack.push(args[0] / args[1]);
+          break;
+        case "+":
+          stack.push(args[0] + args[1]);
+          break;
+        case "-":
+          stack.push(args[0] - args[1]);
+          break;
+
+        // exponents
+        case "^":
+          stack.push(Math.pow(args[0], args[1]));
+          break;
+
+        /* TRIG FUNCTIONS */
+        case "sin":
+          stack.push(Math.sin(args[0]));
+          break;
+        case "cos":
+          stack.push(Math.cos(args[0]));
+          break;
+        case "tan":
+          stack.push(Math.tan(args[0]));
+          break;
+        case "sec":
+          stack.push(1 / Math.cos(args[0]));
+          break;
+        case "csc":
+          stack.push(1 / Math.sin(args[0]));
+          break;
+        case "cot":
+          stack.push(1 / Math.tan(args[0]));
+          break;
+        case "sinh":
+          stack.push(.5 * (Math.pow(Math.E, args[0]) - Math.pow(Math.E, -args[0])));
+          break;
+        case "cosh":
+          stack.push(.5 * (Math.pow(Math.E, args[0]) + Math.pow(Math.E, -args[0])));
+          break;
+        case "tanh":
+          stack.push((Math.pow(Math.E, 2*args[0]) - 1) / (Math.pow(Math.E, 2*args[0]) + 1));
+          break;
+        case "sech":
+          stack.push(2 / (Math.pow(Math.E, args[0]) + Math.pow(Math.E, -args[0])));
+          break;
+        case "csch":
+          stack.push(2 / (Math.pow(Math.E, args[0]) - Math.pow(Math.E, -args[0])));
+          break;
+        case "coth":
+          stack.push((Math.pow(Math.E, 2*args[0]) + 1) / (Math.pow(Math.E, 2*args[0]) - 1));
+          break;
+
+
+        case "floor":
+          stack.push(Math.floor(args[0]));
+          break;
+        case "ceil":
+          stack.push(Math.ceil(args[0]));
+          break;
+
+        default:
+          // unknown operator; error out
+          return false;
+      }
+    }
+  });
+
+  return stack.pop();
+};
+
+Calc.prototype.latexToInfix = function(latex) {
+  /**
+    * function: converts latex notation to infix notation (human-readable, to be converted
+    * again to prefix in order to be processed
+    *
+    * Supported functions / operators / notation:
+    * parentheses, exponents, adding, subtracting, multipling, dividing, fractions
+    * trigonometric (including hyperbolic) functions, floor, ceil
+    */
+
+  var infix = latex;
+
+  infix = infix
+    .replace(/\\frac{([^}]+)}{([^}]+)}/g, "($1)/($2)") // fractions
+    .replace(/\\left\(/g, "(") // open parenthesis
+    .replace(/\\right\)/g, ")") // close parenthesis
+    .replace(/[^\(](floor|ceil|(sin|cos|tan|sec|csc|cot)h?)\(([^\(\)]+)\)[^\)]/g, "($&)") // functions
+    .replace(/([^(floor|ceil|(sin|cos|tan|sec|csc|cot)h?|\+|\-|\*|\/)])\(/g, "$1*(")
+    .replace(/\)([\w])/g, ")*$1")
+    .replace(/([0-9])([A-Za-z])/g, "$1*$2")
+  ;
+
+  return infix;
+};
+
+
+
+
+
+
+
+//_-------------------------------------------------------------------------------------------------------------
+
+
+
 
 
 
@@ -97,57 +387,26 @@ function unknown(obj){
     
     if (clickable == true) {
 	clickable = false;
-	var clickedObject; 
 
+	//checks the clicked object and matches it with the objects in formulas.
+	var clickedObject; 
 	for (var i = 0; i < formulas.length; i++) {
 
 	    for (var j = 0; j < formulas[i].obj.length; j++) {
 
-		
 		if (formulas[i].obj[j] == obj) {
 		    clickedObject = formulas[i];
-
 		    clickedObject.selectedId = clickedObject.obj[j].id;
 		}
 		
 	    }
-
 	    
 	}
-	
-	
-	// formulas.forEach(element => if (element == formulas[i].obj) {
-	    
-	// })
 
 	obj.classList.add("toggle");
 	var variable = obj.children[0].className;
-	var formula = variable.split(" ")[1];
-	variable = variable.split(" ")[0];
-	var nr = obj.parentElement.parentElement.id;
 
-	// console.log(obj.children[0].className);
-	// console.log("hej");
-	// console.log(variable);
-
-	// console.log(variable);
-	// console.log(formula);
-
-
-
-	// console.log(clickedObject.formula);
-	// console.log(variable);
-	// console.log(clickedObject);
-	
 	clickedObject.getInput(variable);
-	
-	// switch (formula){
-
-	// case "einstein":
-	//     // console.log("Einsteincheck");
-	//     einstein(nr, obj, variable);
-	
-	// }
 	
     } else {
 	obj.classList.remove("toggle");
@@ -159,34 +418,7 @@ function unknown(obj){
 }
 
 
-function einstein(nr, obj, variable){
-
-    const c = 299792458;
-    
-    if (variable == "E") {
-	// getInput();
-	var m = getInput(nr, obj, variable);
-	return m * c^2;
-
-	 
-    } else if (variable == "m") {
-	var E = getInput(nr, obj, variable);
-	return E / c^2;
-    } 
-    return 1;
-}
-
-
-// function getInput(nr, obj, variable){
-
-//     if (clickable == false) {
-// 	document.getElementById(nr).insertAdjacentHTML("beforeend", '<div id="temp">  <input type="text" class="tempinput" id="tempinput" name="formula"><button class="tempbutton" onclick="removeField('+ obj.id +')">Lös för '+ variable +' </button> </div>');
-//     } 
-
-	
-// }
-
-
+//Removes
 function removeField(item){
     
     for (var i = 0; i < item.length; i++) {
@@ -198,6 +430,15 @@ function removeField(item){
 
     document.getElementById("temp").remove();
     clickable = true;
+
+    formulas[1].formulaLatex(content);
+
+    
+    
+    // return content;
+
+
+    
 }
 
 
