60420119331fa91aafeac778bf4dbff1a450eb70
snurkeburk <70603096+snurkeburk@users.noreply.github.com>
2021-05-03 13:25:56 +0200

    av-centrerade FAQ text, bytte knappar mot switch, fixade bilder till vit färg samt texten brevid.

--- js/mode.js
+++ js/mode.js
@@ -1,10 +1,19 @@
+
+
 function enableDark() { //bilder mst man göra om i typ PS
     document.body.style.backgroundColor = "	#151521";
     var h1Elements = document.getElementsByTagName("h1");
+    var h2Elements = document.getElementsByTagName("h2");
+    var aElements = document.getElementsByTagName("a");
     let boxFysik = document.getElementById("fysik-ämne");
     let box = document.getElementById("matte-ämne");
     let outline = document.getElementById("outLine");
     let faq = document.getElementById("faqId");
+    let picMatte = document.getElementById("mattePic");
+    let picFysik = document.getElementById("fysikPic");
+    picMatte.style.filter = "invert(100%)"
+    picFysik.style.filter = "invert(100%)"
+
     outline.style.borderColor = "#626294"
     box.style.borderColor = "#626294"
     box.style.backgroundColor ="#1b1b2e";
@@ -12,10 +21,17 @@ function enableDark() { //bilder mst man göra om i typ PS
     boxFysik.style.backgroundColor ="#1b1b2e";
     faq.style.backgroundColor = "#1f1f30";
 
+    for(var i = 0; i < aElements.length; i++) {
+        aElements[i].style.backgroundColor = "#626294";
+        aElements[i].style.color = "#FFF";
+
+    }
     for(var i = 0; i < h1Elements.length; i++) {
         h1Elements[i].style.color = "#fff";
     }
-
+    for(var i = 0; i < h2Elements.length; i++) {
+        h2Elements[i].style.color = "#fff";
+    }
     var pElements = document.getElementsByTagName("p");
 
     for(var i = 0; i < pElements.length; i++) {
@@ -33,13 +49,24 @@ function enableDark() { //bilder mst man göra om i typ PS
     for(var i = 0; i < h3Elements.length; i++) {
         h3Elements[i].style.color = "#fff";
     }
+
+
+    const cb = document.getElementById("check");
+    if (cb.checked != true){
+        enableLight();
+    }
 }   
 
 
 
 function enableLight() {
+    let picMatte = document.getElementById("mattePic");
+    let picFysik = document.getElementById("fysikPic");
+    picMatte.style.filter = "invert(0%)"
+    picFysik.style.filter = "invert(0%)"
     document.body.style.backgroundColor = "	#FFF";
     var h1Elements = document.getElementsByTagName("h1");
+    var aElements = document.getElementsByTagName("a");
     let boxFysik = document.getElementById("fysik-ämne");
     let box = document.getElementById("matte-ämne");
     let outline = document.getElementById("outLine");
@@ -50,6 +77,13 @@ function enableLight() {
     boxFysik.style.borderColor = "#000"
     boxFysik.style.backgroundColor ="#FFF";
     faq.style.backgroundColor = "#FFF";
+
+
+    for(var i = 0; i < aElements.length; i++) {
+        aElements[i].style.backgroundColor = "#f1f5fd";
+        aElements[i].style.color = "#000";
+
+    } 
 
     for(var i = 0; i < h1Elements.length; i++) {
         h1Elements[i].style.color = "#000";
